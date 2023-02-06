687606a3fc5b086a1905705cc4a109ef7e1a6ef9
Timur <timurb070204@gmail.com>
2023-02-07 01:16:40 +0600

    string_formatting_example 3

--- string_formatting.py
+++ string_formatting.py
@@ -1,3 +1,5 @@
 price = 49
-txt = "The price is {:.2f} dollars"
-print(txt.format(price))
+itemno = 3
+count = 14
+txt = "The price is {:.2f} dollars, ID is {}, there are {} pieces"
+print(txt.format(price, itemno, count))
