30728dcbc04f0dcc05ebe8d70893210ae8e2a238
Timur <timurb070204@gmail.com>
2023-02-07 02:45:43 +0600

    for_loops 11

--- for_loops.py
+++ for_loops.py
@@ -1,5 +1,6 @@
-for x in range(6):
-  if x == 3: break
-  print(x)
-else:
-  print("Finally finished!")
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
