0bca9bf4592bdfe876b71db2f30e362520ee0c81
Timur <timurb070204@gmail.com>
2023-02-07 00:04:11 +0600

    numbers_example 6

--- numbers.py
+++ numbers.py
@@ -1,7 +1,15 @@
-x = 3+5j
-y = 5j
-z = -5j
+x = 1
+y = 2.8
+z = 1j
 
-print(type(x))
-print(type(y))
-print(type(z))
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
