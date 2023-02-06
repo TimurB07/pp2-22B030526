007fc3ec2b16705b0d1b3c81e9fb3e471ff5df3e
Timur <timurb070204@gmail.com>
2023-02-07 01:19:31 +0600

    string_formatting_example 7

--- string_formatting.py
+++ string_formatting.py
@@ -1,4 +1,2 @@
-age = 36
-name = "John"
-txt = "His name is {1}. {1} is {0} years old."
-print(txt.format(age, name))
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
