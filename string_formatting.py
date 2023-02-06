ad850578fee6daf569a594774452ada50e53ecd9
Timur <timurb070204@gmail.com>
2023-02-07 01:18:47 +0600

    string_formatting_example 6

--- string_formatting.py
+++ string_formatting.py
@@ -1,5 +1,4 @@
-quantity = 3
-itemno = 567
-price = 49
-myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
-print(myorder.format(quantity, itemno, price))
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
