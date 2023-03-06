da85531b6a308aea5994d52fdb6d0c2e9d40f19c
Timur <timurb070204@gmail.com>
2023-03-07 01:58:50 +0600

    Generator 2

--- generator.py
+++ generator.py
@@ -1,8 +1,9 @@
-def squares_generator(n):
-    for i in range(n):
-        yield i ** 2
+def even_gen(n):
+    for i in range(n+1):
+        if i % 2 == 0:
+            yield i
 
-n = int(input())
-squares = squares_generator(n)
-for square in squares:
-    print(square)
+n = int(input(" "))
+even = even_gen(n)
+result = ", ".join(str(number) for number in even)
+print("{}:{}".format(n, result))
