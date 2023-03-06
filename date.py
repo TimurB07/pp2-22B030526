be3fa0bba833dcddfcfed1c6e31b57bf051df14c
Timur <timurb070204@gmail.com>
2023-03-07 00:30:29 +0600

    Date 4

--- date.py
+++ date.py
@@ -1,4 +1,5 @@
 import datetime
-now=datetime.datetime.now()
-mcr=now.replace(microsecond=0)
-print(mcr)
+date1 = datetime.datetime(2023, 2, 7)
+date2 = datetime.datetime(2022, 12, 7)
+dif = (date1 - date2).total_seconds()
+print(dif)
