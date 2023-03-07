08ce9fa61d3c9277a078129ef6464f8da59522ca
Timur <timurb070204@gmail.com>
2023-03-07 15:18:07 +0600

    Regex 7

--- regex.py
+++ regex.py
@@ -1,6 +1,3 @@
 import re
-def read_file(fpath:str):
-    with open(fpath, mode='r', encoding='utf8') as f:
-        return f.read()
-file=read_file("row.txt")
-print(re.sub("[\n,.]", ":", file))
+str=input()
+print(re.sub('(?:^|_)([a-z])', lambda x: x.group(1).upper(), str))
