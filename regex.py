b2daa5efb31f37b8624dcdc60a803ae4426700ba
Timur <timurb070204@gmail.com>
2023-03-07 14:49:32 +0600

    Regex 4

--- regex.py
+++ regex.py
@@ -3,5 +3,5 @@ def read_file(fpath:str):
     with open(fpath, mode='r', encoding='utf8') as f:
         return f.read()
 file=read_file("row.txt")
-pattern = "[а-б]+(_[а-б]+)+"
+pattern = '[А-Я][а-я]+'
 print(re.findall(pattern, file))
