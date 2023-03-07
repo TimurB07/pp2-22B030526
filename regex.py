027590667dbdb33cf255cadbdf0c0572fb2c8a60
Timur <timurb070204@gmail.com>
2023-03-07 14:10:03 +0600

    Regex 1

---
+++ regex.py
@@ -0,0 +1,7 @@
+import re
+def read_file(fpath:str):
+    with open(fpath, mode='r', encoding='utf8') as f:
+        return f.read()
+file=read_file("row.txt")
+pattern="аб*"
+print(re.findall(pattern, file))
